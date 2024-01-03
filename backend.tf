179e6d8bf1a500941b211b87c7338e105697f936
snehad1994 <151536782+snehad1994@users.noreply.github.com>
2024-01-03 11:25:17 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "mydev-tf-state-bucket-project-terraform-batch-21"
+    bucket = "project-jenkin"
     key = "main"
     region = "us-east-1"
     dynamodb_table = "my-dynamodb-table"
